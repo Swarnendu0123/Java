dbc4f71eca6d02d8c64c704c030a64b8c98f4a14
Swarnendu0123 <clickswarnendu123@gmail.com>
2023-05-17 23:57:06 +0530

    function added

--- Binary Trees/binaryTreesB.java
+++ Binary Trees/binaryTreesB.java
@@ -1,5 +1,7 @@
 import java.util.*;
 
+import javax.lang.model.type.ErrorType;
+
 public class binaryTreesB {
     static class Node {
         int data;
@@ -236,18 +238,106 @@ public class binaryTreesB {
         System.out.println();
     }
 
+    // function to calculate lowest common Ancestor(2nd approch)
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+        Node leftlca = lca2(root.left, n1, n2);
+        Node rightlca = lca2(root.right, n1, n2);
+        if (rightlca == null) {
+            return leftlca;
+        }
+        if (leftlca == null) {
+            return rightlca;
+        }
+        return root;
+    }
+
+    // helping function
+    public static int lcadist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = lcadist(root.left, n);
+        int rightDist = lcadist(root.right, n);
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    // function to calculate min distance b/w two nodes
+    public static int minDist(Node root, int n1, int n2) {
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcadist(lca, n1);
+        int dist2 = lcadist(lca, n2);
+        return dist1 + dist2;
+    }
+
+    // function to find node
+    public static Node findNode(Node root, int n) {
+        if (root.data == n || root == null) {
+            return root;
+        }
+        Node ln = findNode(root.left, n);
+        Node rn = findNode(root.right, n);
+        if (ln == null) {
+            return rn;
+        } else {
+            return ln;
+        }
+    }
+
+    // Kth ancestor of a node
+    // 1
+    // / \
+    // 2 3
+    // /\ /\
+    // 4 5 6 7
+    public static int KthAncestor(Node root, int n, int k) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = KthAncestor(root.left, n, k);
+        int rightDist = KthAncestor(root.right, n, k);
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        }
+        int max = Math.max(leftDist, rightDist);
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+        return max + 1;
+    }
+
     public static void main(String[] args) {
         // main tree
+        // 1
+        // / \
+        // 2 3
+        // /\ /\
+        // 4 5 6 7
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        root.right.right = new Node(6);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
         // sub tree
         Node subroot = new Node(2);
         subroot.left = new Node(4);
         subroot.right = new Node(5);
-        topView(root);
+        System.out.println(minDist(root, 2, 4));
     }
 }
