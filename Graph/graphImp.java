e3664d49ee2e85854c8f58dfc4c9520e6f5846de
Swarnendu0123 <clickswarnendu123@gmail.com>
2023-06-20 16:17:18 +0530

    is cycle function added

--- Graph/graphImp.java
+++ Graph/graphImp.java
@@ -24,8 +24,16 @@ public class graphImp {
     // BFS || Time Complixity - O (V + E)
     // V = vertices E = edges
     public static void BFS(ArrayList<Edge> graph[]) {
-        Queue<Integer> q = new LinkedList<>();
         boolean visited[] = new boolean[graph.length];
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                BFSutil(graph, visited);
+            }
+        }
+    }
+
+    public static void BFSutil(ArrayList<Edge> graph[], boolean visited[]) {
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);// source
         while (!q.isEmpty()) {
             int curr = q.remove();
@@ -42,13 +50,22 @@ public class graphImp {
 
     // DFS || Time Complixity - O (V + E)
     // V = vertices E = edges
-    public static void DFS(ArrayList<Edge> graph[], int curr, boolean visited[]) {
+    public static void DFS(ArrayList<Edge> graph[]) {
+        boolean visited[] = new boolean[graph.length];
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
+                DFSutil(graph, i, visited);
+            }
+        }
+    }
+
+    public static void DFSutil(ArrayList<Edge> graph[], int curr, boolean visited[]) {
         System.out.print(curr + " ");
         visited[curr] = true;
         for (int i = 0; i < graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
             if (!visited[e.dest]) {
-                DFS(graph, e.dest, visited);
+                DFSutil(graph, e.dest, visited);
             }
         }
     }
@@ -73,11 +90,42 @@ public class graphImp {
         return false;
     }
 
+    // function to detect circle in a graph
+    public static boolean isCycle(ArrayList<Edge> graph[]) {
+        boolean[] visited = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i] && isCycleUtil(graph, i, visited, -1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // helping function for is cycle
+    public static boolean isCycleUtil(ArrayList<Edge> graph[], int curr, boolean visited[], int par) {
+        visited[curr] = true;
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!visited[e.dest]) {
+                if (isCycleUtil(graph, e.dest, visited, curr)) {
+                    return true;
+                }
+            } else if (visited[curr] && par != e.dest) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
+        ArrayList<Edge> graph1[] = new ArrayList[3];
         for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < graph1.length; i++) {
+            graph1[i] = new ArrayList<>();
         }
         graph[0].add(new Edge(0, 1));
         graph[0].add(new Edge(0, 2));
@@ -102,8 +150,27 @@ public class graphImp {
 
         graph[6].add(new Edge(6, 5));
 
-        ArrayList<Integer> path = new ArrayList<>();
-        System.out.println(hasPath(graph, 0, new boolean[graph.length], 6, path));
-        System.out.println(path);
+        // another graph
+        graph1[0].add(new Edge(0, 1));
+        graph1[0].add(new Edge(0, 2));
+
+        graph1[1].add(new Edge(1, 0));
+        graph1[1].add(new Edge(1, 2));
+
+        graph1[2].add(new Edge(2, 0));
+        graph1[2].add(new Edge(2, 1));
+
+        // ArrayList<Integer> path = new ArrayList<>();
+        // boolean check = hasPath(graph, 0, new boolean[graph.length], 6, path);
+        // System.out.println(check);
+        // if (check) {
+        // int i;
+        // System.out.print("One path: ");
+        // for (i = path.size() - 1; i > 0; i--) {
+        // System.out.print(path.get(i) + "->");
+        // }
+        // System.out.print(path.get(i));
+        // }
+        System.out.println(isCycle(graph1));
     }
 }
