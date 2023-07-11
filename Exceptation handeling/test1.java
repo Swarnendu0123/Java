765d625c3b498703e4f1ef6841221b7ebc9df99d
Swarnendu0123 <clickswarnendu123@gmail.com>
2023-07-11 15:37:40 +0530

    ...

--- Exceptation handeling/test1.java
+++ Exceptation handeling/test1.java
@@ -1,23 +1,13 @@
-class Demo {
-    public void a() {
-        try {
-            b();
-        } catch (Exception e) {
-            System.out.println(e.getStackTrace());
-        }
-    }
-
-    void b() throws Exception {
-        int num1 = 10;
-        int num2 = 0;
-        int result = num1 / num2;
-        System.out.println(result);
-    }
-}
+import java.util.*;
 
 public class test1 {
     public static void main(String[] args) {
-        Demo d = new Demo();
-        d.a();
+        int name = 0;
+        try (Scanner in = new Scanner(System.in)) {
+            name = in.nextInt();
+        } catch (Exception e) {
+            System.out.println("some error occured");
+        }
+        System.out.println(name);
     }
 }
---
+++ Exceptation handeling/test3.java
@@ -0,0 +1,17 @@
+public class test3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = -2;
+        try {
+            if (num2 < 0) {
+                Exception e = new ArithmeticException();
+                throw e;
+            }
+            int result = num1 / num2;
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Enter a valid number");
+            System.out.println("Error: " + e);
+        }
+    }
+}
